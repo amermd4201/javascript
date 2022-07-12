10031cfeab392a7c5a887f61abdd43af84658032
Amer Mohammad <am21418@corp.ojas-it.com>
2022-07-12 11:54:02 +0530

    js

---
+++ Practice/variables.js
@@ -0,0 +1,33 @@
+// var uname="My  Name is khan";
+// var what="Hello amer \t"+uname+"How Are you"
+// console.log(what);
+
+// Appending variables to Strings
+// var asdf="Beautiful";
+// var fdsa="code camp";
+// console.log(fdsa);
+// fdsa+=asdf;
+// console.log(fdsa);
+
+//Length of thwe string
+//  var firstNameLength=0;
+//  var firstName="Lovelace";
+//  firstNameLength=firstName.length;
+//  console.log("length = "+firstNameLength)
+
+ //Bracket Notation to Find first character in String
+ //means finding the letter at the position
+//  var firstLetterOfLastName="";
+//  var lastName="Lovelace";
+//  firstLetterOfLastName=lastName[05];
+//  console.log(firstLetterOfLastName)
+
+//String Immutability
+// var myStr="Jello World";
+// myStr[0]="H"; // its not working because immutability 
+// myStr="Hello World";
+// console.log(myStr);
+
+//Find nth Character in String
+var firstName="Durga";
+var lastLetterOfFirstname=firstName[firstName.length-1]
